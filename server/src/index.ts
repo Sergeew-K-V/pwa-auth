21c10e4da02de020a7a1e978cacd63128b6dbae8
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-15 18:00:44 +0300

    init next

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,6 +18,7 @@ app.use(cors());
 async function startServer() {
   try {
     if (process.env.CLUSTER) {
+      mongoose.set("strictQuery", false);
       await mongoose.connect(process.env.CLUSTER);
 
       app.listen(PORT, () => {
