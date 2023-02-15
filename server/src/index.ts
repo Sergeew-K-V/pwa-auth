4c7bcfeb9dee567e0c1ff77662ce1e69b0a7e2d3
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-15 15:57:22 +0300

    init servers libraries and scripts

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import path from "path";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+async function startServer() {
+  try {
+    // if (process.env.CLUSTER) {
+    // await mongoose.connect(process.env.CLUSTER);
+
+    app.listen(PORT, () => {
+      console.log("Server started on PORT: ", PORT);
+    });
+    // }
+  } catch (error) {
+    return console.log(`Server have some errors: ${error}`);
+  }
+}
+
+startServer();
+
+export default app;
