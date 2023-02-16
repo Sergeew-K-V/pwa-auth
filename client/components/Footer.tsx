9d2cf03b1ef718e5b57197049893777465a76dda
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-16 16:32:05 +0300

    styles

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Footer = (): JSX.Element => {
   return (
-    <div className='grid grid-cols-2'>
+    <div className='grid grid-cols-2 sticky items-center gap-x-4 justify-items-center'>
       <div>Copyright © 2023 Fobbelend, Inc. All rights reserved</div>
       <div>
         <Image className='w-12' width={100} height={100} alt={'logo'} src={'/LogoKitchen.svg'} />
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link'
 
 const Header = (): JSX.Element => {
   return (
-    <div className='grid grid-cols-3 sticky items-center'>
-      <div>
-        <Image className='w-16' src={'/LogoKitchen.svg'} alt='search' width={100} height={100} />
+    <div className='grid grid-cols-3 sticky items-center gap-x-4 top-0'>
+      <div className='ml-8'>
+        <Link href={'/'}>
+          <Image className='w-16' src={'/LogoKitchen.svg'} alt='search' width={100} height={100} />
+        </Link>
       </div>
       <div>
         <nav>
@@ -28,7 +30,7 @@ const Header = (): JSX.Element => {
           </ul>
         </nav>
       </div>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-3 gap-x-8'>
         <div>
           <select className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
             <option selected>Русский</option>
@@ -43,19 +45,19 @@ const Header = (): JSX.Element => {
             Search
           </label>
           <div className='relative items-center'>
-            <input
-              type='search'
-              id='search'
-              className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-              placeholder='Search'
-              required
-            />
             <Image
-              className='w-6 absolute right-1 top-2'
+              className='w-5 absolute left-2 top-1/2 -translate-y-1/2'
               src={'/magnifying-glass-backup-svgrepo-com.svg'}
               alt='search'
               width={100}
               height={100}
+            />
+            <input
+              type='search'
+              id='search'
+              className='block w-full p-2 pl-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder='Search'
+              required
             />
           </div>
         </div>
