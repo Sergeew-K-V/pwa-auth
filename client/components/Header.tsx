55b8cbf9316b20bc37181d37c9ac8f88247d8dd7
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-16 15:48:57 +0300

    feat: added select, input

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Header = (): JSX.Element => {
   return (
-    <div className='grid grid-cols-3 sticky w-full'>
+    <div className='grid grid-cols-3 sticky items-center'>
       <div>
         <Image className='w-16' src={'/LogoKitchen.svg'} alt='search' width={100} height={100} />
       </div>
@@ -11,35 +11,53 @@ const Header = (): JSX.Element => {
         <nav>
           <ul className='grid grid-cols-3'>
             <li>
-              <Link href='/'>Home</Link>
+              <Link className='font-medium hover:underline' href='/'>
+                Home
+              </Link>
             </li>
             <li>
-              <Link href='/gallery'>Gallery</Link>
+              <Link className='font-medium hover:underline' href='/gallery'>
+                Gallery
+              </Link>
             </li>
             <li>
-              <Link href='/account'>Account</Link>
+              <Link className='font-medium hover:underline' href='/account'>
+                Account
+              </Link>
             </li>
           </ul>
         </nav>
       </div>
       <div className='grid grid-cols-3'>
         <div>
-          <select>
-            <option value='ru' selected>
-              Русский
-            </option>
-            <option value='en'>English</option>
+          <select className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+            <option selected>Русский</option>
+            <option value='US'>English</option>
           </select>
         </div>
-        <div className='grid grid-cols-2'>
-          <input type='text' placeholder='Sofa...' />
-          <Image
-            className='w-6'
-            src={'/magnifying-glass-backup-svgrepo-com.svg'}
-            alt='search'
-            width={100}
-            height={100}
-          />
+        <div>
+          <label
+            htmlFor='search'
+            className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'
+          >
+            Search
+          </label>
+          <div className='relative items-center'>
+            <input
+              type='search'
+              id='search'
+              className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder='Search'
+              required
+            />
+            <Image
+              className='w-6 absolute right-1 top-2'
+              src={'/magnifying-glass-backup-svgrepo-com.svg'}
+              alt='search'
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
         <div>
           <button>
